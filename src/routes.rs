a9b9d8f175e20265fb4d8d09c5e55f2ed1dc4487
Kaisei Yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2019-09-20 15:17:15 +0900

    impl register

---
+++ src/routes.rs
@@ -0,0 +1,31 @@
+use crate::prelude::*;
+
+pub mod register {
+    use super::*;
+    use rocket::{
+        http::Status,
+        response::status::Custom,
+    };
+    use rocket_contrib::templates::Template;
+
+    #[post("/", data = "<string>")]
+    pub fn register(string: String) -> Result<Status, Custom<Template>> {
+        let mut data = read_data().map_err(|e| {
+            unimplemented!()
+            // todo 500 Internal Server Error
+        })?;
+
+        let register: Register = serde_json::from_str(&string).map_err(|e| {
+            unimplemented!()
+            // todo 500 Internal Server Error
+        })?;
+
+        let book = Book::from(register);
+
+        data.push(book);
+
+        write_data(&data)?;
+
+        Ok(Status::Ok)
+    }
+}
